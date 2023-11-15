9e6f077cabb004c4ccf131b81e15a1edb971cd56
Darlinton Luis Siqueira <darlinton2000@gmail.com>
2023-11-15 19:29:14 -0300

    Criando as Rotas

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\BarberController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ping', function() {
     return ['pong' => true];
 });
+
+// AuthController
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
+Route::post('/auth/refresh', [AuthController::class, 'refresh']);
+Route::post('/user', [AuthController::class, 'create']);
+
+// UserController
+Route::get('/user', [UserController::class, 'read']);
+Route::put('/user', [UserController::class, 'update']);
+Route::get('/user/favorites', [UserController::class, 'getFavorites']);
+Route::post('/user/favorite', [UserController::class, 'addFavorite']);
+Route::get('/user/appointments', [UserController::class, 'getAppointments']);
+
+// BarberController
+Route::get('/barbers', [BarberController::class, 'list']);
+Route::get('/barber/{id}', [BarberController::class, 'one']);
+Route::post('/barber/{id}/appointment', [BarberController::class, 'setAppointment']);
+Route::get('/search', [BarberController::class, 'search']);
